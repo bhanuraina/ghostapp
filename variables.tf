c7532a3466abc1aa36cdb2d0f3430462f027aeef
bhanuraina <bhanuraina@gmail.com>
2022-02-22 21:29:05 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "website_url" {
+  type    = string
+  default = "http://{your_domain}"
+}
+
+variable "website_admin_url" {
+  type    = string
+  default = "http://admin.{your_domain}"
+}
