e7aad3ea0ea3e2b5caafda94623c152cb0a6f0ff
bhanuraina <bhanuraina@gmail.com>
2022-02-22 21:54:21 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,32 @@ variable "website_admin_url" {
   type    = string
   default = "http://admin.{your_domain}"
 }
+
+variable "region" {
+  type    = string
+  default = "eu-west-3"
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "public_subnets_cidr" {
+  type = list(string)
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "private_subnets_cidr" {
+  type = list(string)
+}
