54383ee094357fd5e7a434ddd3d8cf632bd5ee38
bhanuraina <bhanuraina@gmail.com>
2022-02-22 23:43:54 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,25 +17,6 @@ variable "vpc_cidr" {
   type = string
 }
 
-variable "name" {
-  type = string
-}
-
-variable "env" {
-  type = string
-}
-
-variable "public_subnets_cidr" {
-  type = list(string)
-}
-
-variable "availability_zones" {
-  type = list(string)
-}
-
-variable "private_subnets_cidr" {
-  type = list(string)
-}
 variable "ec2_instance_type" {
 default = "t3.micro"
 }
