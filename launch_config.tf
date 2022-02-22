55ffae6d047337357cee99f0ffac289f4a6cc3d4
bhanuraina <bhanuraina@gmail.com>
2022-02-22 21:28:13 +0100

    Update launch_config.tf

--- launch_config.tf
+++ launch_config.tf
@@ -3,9 +3,18 @@ resource "aws_launch_configuration" "ghost_lc" {
   image_id             = data.aws_ami.ubuntu.image_id
   security_groups      = [aws_security_group.ghost_asg_sg.id]
   instance_type        = var.ec2_instance_type
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name # here
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  #  path to the user data file
+  user_data = templatefile("${path.module}/user_data/ghost_init.sh",
+    {
+      # This is pulled from the rds resource created in rds.tf
+      "endpoint" = aws_db_instance.default.address,
+      "database" = aws_db_instance.default.name,
+      "username" = aws_db_instance.default.username,
+      # !!! Remember to find a secure way to retrieve your password
+      "password"  = var.mysql_password,
+      "admin_url" = var.website_admin_url,
+      "url"       = var.website_url
+    }
+  )
 }
