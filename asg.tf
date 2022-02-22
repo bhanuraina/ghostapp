b87712fc40db9b9bfb9ef54d2a0380e1289ba845
bhanuraina <bhanuraina@gmail.com>
2022-02-22 23:04:33 +0100

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -1,7 +1,7 @@
 resource "aws_elb" "web_elb" {
   name = "web-elb"
   security_groups = [
-    aws_security_group.ghost_asg_sg.id
+    aws_security_group.elb_http.id
   ]
   subnets = [
     aws_subnet.public_us_east_1a.id,
@@ -25,4 +25,28 @@ resource "aws_elb" "web_elb" {
     instance_protocol = "http"
   }
 
+}
+
+resource "aws_security_group" "elb_http" {
+  name        = "elb_http"
+  description = "Allow HTTP traffic to instances through Elastic Load Balancer"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow HTTP through ELB Security Group"
+  }
 }
