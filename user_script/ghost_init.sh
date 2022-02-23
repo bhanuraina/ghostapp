777608f041eb1ddb9b58264311997a95c63287ae
bhanuraina <bhanuraina@gmail.com>
2022-02-23 20:03:04 +0100

    Add files via upload

--- user_script/ghost_init.sh
+++ user_script/ghost_init.sh
@@ -34,3 +34,6 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
         --dbname "${database}" \
         --process systemd \
         --no-prompt 
+    
+    sleep 600
+    ghost restart
