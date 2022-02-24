93f06e994ff69e470867e006d62ac313669a116e
bhanuraina <bhanuraina@gmail.com>
2022-02-24 18:20:18 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -162,7 +162,7 @@ variable "override_s3_bucket_name" {
 variable "s3_bucket_name" {
   description = "If override_s3_bucket_name is true, use this bucket name for replica instead of dynamic name with bucket_prefix"
   type        = string
-  default     = "ghost"
+  default     = "ghost114455"
 }
 variable "s3_bucket_name_replica" {
   description = "If override_s3_bucket_name is true, use this bucket name instead of dynamic name with bucket_prefix"
