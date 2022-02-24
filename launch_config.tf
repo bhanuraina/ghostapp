aef755daccd88d3ac57db13199276b8b5550ee5e
bhanuraina <bhanuraina@gmail.com>
2022-02-24 17:01:04 +0100

    Update launch_config.tf

--- launch_config.tf
+++ launch_config.tf
@@ -21,8 +21,7 @@ resource "aws_launch_configuration" "ghost_lc" {
   )
 }
 resource "aws_key_pair" "this" {
-  count = var.create_key_pair ? 1 : 0
-
+  
   key_name        = "cloud"
   public_key      = tls_private_key.this.public_key_openssh
 
