e840805bbe47a1d9b26414d26e6afa379fcfb232
bhanuraina <bhanuraina@gmail.com>
2022-02-25 11:06:21 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 terraform {
   backend "s3" {
     bucket         = var.s3_bucket_name
-    key            =  var.s3_bucket_name
+    key            = var.s3_bucket_name
     region         = var.region
     encrypt        = true
     kms_key_id     = aws_kms_key.this.arn
