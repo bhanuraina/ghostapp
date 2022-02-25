bb41c3e6b3c2350ab7eea5314bb8828a41a6be8c
bhanuraina <bhanuraina@gmail.com>
2022-02-25 11:15:43 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,11 +3,10 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = aws_s3_bucket.sate.id
-    key            = aws_s3_bucket.sate.id
+    bucket         = "ghostss11445566"
+    key            = "ghostss11445566"
     region         = us-east-1
     encrypt        = true
-    kms_key_id     = aws_kms_key.this.arn
-    dynamodb_table = aws_dynamodb_table.lock.id
+    dynamodb_table = "tf-remote-state-lock"
   }
 }
