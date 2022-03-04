36ad2a4d33485b4188d5f4800128fa5ad1fd54ff
bhanuraina <bhanuraina@gmail.com>
2022-03-04 18:11:40 +0100

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  db_name                 = "mydb"
+  db_name              = var.mysql_dbname
   username             = var.mysql_username
   password             = var.mysql_password
   parameter_group_name = "default.mysql5.7"
