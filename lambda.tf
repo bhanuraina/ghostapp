82e840529c6a2fc02d8f862f72560cf96980a1d8
bhanuraina <bhanuraina@gmail.com>
2022-03-04 14:34:23 +0100

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -27,7 +27,15 @@ resource "aws_lambda_function" "ghost_lambda" {
   source_code_hash = filebase64sha256("delete_post-a13f3829-2554-49c2-ae71-84915b4b8201.zip")
 
   runtime = "python3.8"
-
+  vpc_config {
+           security_group_ids = [
+               "aws_security_group.ghost_asg_sg.id",
+            ]
+           subnet_ids         = [
+               aws_subnet.public_us_east_1a.id,
+               aws_subnet.public_us_east_1b.id
+            ]
+        }
   environment {
     variables = {
       Name  = "ghost"
